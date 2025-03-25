ae702a909c1e2e95ebd60d364fec1a32b351b0f8
rrazban <rmr4442@gmail.com>
2025-03-25 11:54:10 -0400

    add results

---
+++ predFC/compare.py
@@ -0,0 +1,42 @@
+'''
+Compare results from transformer architecture to deep 
+learning approach published by Sarwar et al. 2021.
+
+Note that I ran Sarwar's publicly available code on the 
+exact same `hcp.mat` input dataset and provided the output
+in `Sarwar2021_hcp_cv10.mat`.
+
+'''
+
+import scipy
+from scipy.stats import pearsonr
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+
+    save_path='hcp_cv10.mat' 
+    out_data = scipy.io.loadmat(save_path)
+    input_data, output_data, pred_data = out_data['in'], out_data['out'], out_data['predicted'] 
+
+    compare_path = 'Sarwar2021_hcp_cv10.mat' 
+    compare_out_data = scipy.io.loadmat(compare_path)
+    compare_pred_data = compare_out_data['predicted'] #input_data, output_data are the same
+    num_subs = pred_data.shape[0]
+
+    outputs = []
+    for i in range(num_subs):
+        corr, pval = pearsonr(pred_data[i], output_data[i])
+        outputs.append([corr, '{0} HCP individuals'.format(num_subs), 'transformer'])
+
+        comp_corr, comp_pval = pearsonr(compare_pred_data[i], output_data[i])
+        outputs.append([comp_corr, '{0} HCP individuals'.format(num_subs), 'DNN (Sarwar 2021)'])
+
+    df = pd.DataFrame(outputs, columns = ['corr', 'subset', 'method'])
+    sns.violinplot(data=df, x='subset', y='corr', hue='method', inner='quart', split=True)
+    plt.xlabel('')
+    plt.ylabel('$r$(predicted FC $-$ empirical FC)')
+    plt.show()
---
+++ predFC/results_summary.md
@@ -0,0 +1,8 @@
+As shown in `results.png`, our transformer architecture approach outperforms a previously published deep neural network approach. We obtain an average Pearson correlation between predicted and empirical functional connectivities of 0.651 ± 0.096, compare to 0.507 ± 0.200 for the DNN approach. The central dashed line in each distribution corresponds to the median, while neighboring dashed lines correspond to lower and upper quartiles, respectively.
+
+An important distinction is that our transformer approach does not attempt to maximally capture individual differences. This could artificially inflate our performance by only capturing average effects among the 1010 individuals.
+
+References:
+* Sarwar, T., Tian, Y., Yeo, B.T., Ramamohanarao, K. and Zalesky, A., 2021. Structure-function coupling in the human connectome: A machine learning approach. NeuroImage, 226, p.117609.
+* Zalesky, A., Sarwar, T., Tian, Y., Liu, Y., Yeo, B.T. and Ramamohanarao, K., 2024. Predicting an individual’s functional connectivity from their structural connectome: Evaluation of evidence, recommendations, and future prospects. Network Neuroscience, 8(4), pp.1291-1309.
+
