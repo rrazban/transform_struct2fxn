8ca2fb3a002e906c6713485bf35bf95b5c699674
rrazban <rmr4442@gmail.com>
2025-03-26 11:14:59 -0400

    make figure labels larger

--- predFC/compare.py
+++ predFC/compare.py
@@ -15,6 +15,31 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+plt.rcParams.update({'font.size': 14})  #uniformly increase fontsize of figure
+
+
+def plotout(df):
+    ax = sns.violinplot(data=df, x='subset', y='corr', hue='machine learning method', inner='quart', split=True)
+    sns.move_legend(ax, "lower left")
+    plt.xlabel('')
+    plt.ylabel('$r$(predicted FC $-$ empirical FC)')
+
+    plt.tight_layout()
+    plt.show()
+
+
+def organize_data(num_subs, output_data, pred_data, compare_pred_data):
+    outputs = []
+    for i in range(num_subs):
+        corr, pval = pearsonr(pred_data[i], output_data[i])
+        outputs.append([corr, '{0} HCP individuals'.format(num_subs), 'transformer'])
+
+        comp_corr, comp_pval = pearsonr(compare_pred_data[i], output_data[i])
+        outputs.append([comp_corr, '{0} HCP individuals'.format(num_subs), 'DNN (Sarwar 2021)'])
+
+    df = pd.DataFrame(outputs, columns = ['corr', 'subset', 'machine learning method'])
+    return df
+
 
 if __name__ == "__main__":
 
@@ -27,16 +52,5 @@ if __name__ == "__main__":
     compare_pred_data = compare_out_data['predicted'] #input_data, output_data are the same
     num_subs = pred_data.shape[0]
 
-    outputs = []
-    for i in range(num_subs):
-        corr, pval = pearsonr(pred_data[i], output_data[i])
-        outputs.append([corr, '{0} HCP individuals'.format(num_subs), 'transformer'])
-
-        comp_corr, comp_pval = pearsonr(compare_pred_data[i], output_data[i])
-        outputs.append([comp_corr, '{0} HCP individuals'.format(num_subs), 'DNN (Sarwar 2021)'])
-
-    df = pd.DataFrame(outputs, columns = ['corr', 'subset', 'method'])
-    sns.violinplot(data=df, x='subset', y='corr', hue='method', inner='quart', split=True)
-    plt.xlabel('')
-    plt.ylabel('$r$(predicted FC $-$ empirical FC)')
-    plt.show()
+    df = organize_data(num_subs, output_data, pred_data, compare_pred_data)
+    plotout(df)
